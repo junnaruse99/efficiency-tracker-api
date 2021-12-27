2892454cd85fcdc76e8e7620e193e31517303e4f
Jun Naruse <jun.naruse@gmail.com>
2021-12-26 19:37:29 -0500

    [ADD] Functionality to update a user

--- controller/userController.js
+++ controller/userController.js
@@ -56,3 +56,68 @@ exports.createUser = async (req, res) => {
         res.status(400).json( { msg: 'There was an error' } );
     }
 }
+
+exports.updateUser = async (req, res) => {
+    // Check if there were any errors
+    const errors = validationResult(req);
+    if( !errors.isEmpty() ) {
+        return res.status(400).json( { errors: errors.array() } );
+    }
+
+    // Extract email and password
+    const { email, password, name } = req.body;
+
+    try {
+        // Check if user is unique
+        let user = await User.findOne( {email} );
+
+        if(user) {
+            console.log("PUT /api/users 400")
+            return res.status(400).json( { msg: 'The user already exists' } );
+        }
+
+        // Get user by email
+        user = await User.findOne({email:req.params.email});
+
+        // Check if user exists
+        if(!user) {
+            console.log("PUT /api/users 404");
+            console.log("User not found");
+            return res.status(404).json( { msg: 'User not found' } );
+        }
+
+        // Hashing password
+        const salt = await bcryptjs.genSalt(10);
+        // Updating values
+        user.email = email;
+        user.name = name;
+        user.password = await bcryptjs.hash(password, salt);
+        
+        // Update user
+        await user.save();
+
+        // Create and sign the JWT
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        // Sign the JWT
+        jwt.sign(payload, process.env.SECRET, {
+            expiresIn: 3600
+        }, (error, token) => {
+            if(error) throw error;
+
+            // Confirmation messagge
+            res.json( { token } );
+        });
+
+        console.log("PUT /api/users 200");
+        
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).json( { msg: 'There was an error' } );
+    } 
+}
